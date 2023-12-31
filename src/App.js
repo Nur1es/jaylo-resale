c93a0166a6e31cc8cfc014b2b3c72a07ec675458
unknown <aimeerimakmatova6@gmail.com>
2023-12-31 22:16:41 +0600

    added text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import AboutUs from './components/aboutUs/AboutUs';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
      {/* <h1>start</h1> */}
      <Header/>
+     <Main/>
+     <AboutUs/>
     </div>
   );
 }
